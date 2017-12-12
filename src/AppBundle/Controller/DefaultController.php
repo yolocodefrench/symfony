87e8d662144c840ab26baec4f44af02519ffb768
kiriancaumes <kirian.caumes@ynov.com>
2017-12-12 13:36:09 +0100

    da shit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,79 +18,10 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
-    }
-    /**
-     * @Route("/multiplication/", name="multiplication")
-     */
-    public function multiplication() {
-        /*return $this->render('default/number.html.twig', [
-            'numberA' => $numberA,
-            'numberB' => $numberB
-        ]);
-        */
-        $user = $this->getDoctrine()
-        ->getRepository('AppBundle:Projet')
-        ->findProjetArray();
-        if (!$user) {
-            throw $this->createNotFoundException(
-                'No product found for id 1'
-            );
-        }
-        return $this->render('default/test.html.twig',[
-            'product' => $user
-        ]);
-    }
-    /**
-     * @Route("/tentation/{essai}", name="test")
-     */
-    public function testAction(){
-        $projets = $this->getDoctrine()
-        ->getRepository('AppBundle:Projet')
-        ->findProjetArray();
-        if (!$projets) {
-            throw $this->createNotFoundException(
-                'No product found for id 1'
-            );
-        }
-        return $this->render('default/test.html.twig',[
-            'product' => $projets 
-        ]);
-    }
-    /**
-     * @Route("/addUser", name="test")
-     */
-    public function formAction(Request $request){
-        
-        $projets = $this->getDoctrine()
-        ->getRepository('AppBundle:Projet')
-        ->findProjetArray();
-        $utilisateur = new Utilisateur();
-        // On crée le FormBuilder grâce au service form factory
-        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $utilisateur);
-        // On ajoute les champs de l'entité que l'on veut à notre formulaire
-        $formBuilder
-          ->add('nom',      TextType::class)
-          ->add('prenom',     TextType::class)
-          ->add('classe_ou_fonction',   TextType::class)
-          ->add('role',    TextType::class)
-          ->add('projet', ChoiceType::class, array(
-            'choices'  => $projets))
-          ->add('save',      SubmitType::class)
-        ;
-        // Pour l'instant, pas de candidatures, catégories, etc., on les gérera plus tard
-        // À partir du formBuilder, on génère le formulaire
-        $form = $formBuilder->getForm();
-        // On passe la méthode createView() du formulaire à la vue
-        // afin qu'elle puisse afficher le formulaire toute seule
-        return $this->render('default/add.html.twig', array(
-          'form' => $form->createView(),
-        ));
+    public function indexAction()
+    {   
+        $this->denyAccessUnlessGranted("ROLE_USER");
+        return $this->render('default/accueil.html.twig');
     }
     
 }
